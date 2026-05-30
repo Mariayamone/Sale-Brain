ba6a4735d7d7189e44b69232b5bdf5a3be3f5f30
Sein Mauk Lin Htet <mgseinmauklinhtet@gmail.com>
2026-05-30 13:22:54 +0630

    Messnger bot final

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
+import FacebookCallback from "./pages/FacebookCallback.tsx";
 import "./index.css";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/facebook/callback" element={<FacebookCallback />} />
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
   </StrictMode>
 );
